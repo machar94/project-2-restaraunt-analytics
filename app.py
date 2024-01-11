b9827953cb36b1b4a6607bef964c7772e09a3a62
Madhav Achar <acharmd@gmail.com>
2024-01-10 19:41:34 -0500

    Start of two tables

--- app.py
+++ app.py
@@ -1,11 +1,15 @@
 # This app is used to format the data from the csv files into a format that can be used by the database
 
-import pandas as pd
+
 import format
+import pandas as pd
+import pytz
 import random
 import string
-from tqdm import tqdm
+import yaml
 
+from tqdm import tqdm
+from typing import Union
 
 primaryKeyLength = 16
 
@@ -15,12 +19,121 @@ primaryKeyLength = 16
 #############
 
 
-def generateRandomString(length=primaryKeyLength) -> str:
+def matchRestaraunt(row: pd.Series, df: pd.DataFrame) -> Union[pd.Series, None]:
+    '''
+    Check if a restaurant already exists in the Restaurant table
     '''
 
+    return None
+
+
+def fillRestaurantTable(tables: dict[str, pd.DataFrame], datasets: dict[str, pd.DataFrame]) -> None:
+    '''
+    Fill the Restaurant table
+    '''
+
+    if 'OpenRestaurantInspections' in datasets:
+        df = datasets['OpenRestaurantInspections']
+
+        to_add, restaurant_ids = [], []
+
+        # Iterate through rows and add to table if restarant does not exist
+        for _, row in tqdm(df.iterrows()):
+
+            if matchRestaraunt(row, tables['Restaurant']):
+                continue
+            else:
+                id = str(generateRandomBits(64))
+                restaurant_ids.append(id)
+
+                to_add.append({
+                    'ID': id,
+                    'Name': row['RestaurantName'],
+                    'LegalBusinessName': row['FormattedLegalBusinessName'],
+                    'StreetAddress': row['FormattedBusinessAddress'],
+                    'Borough': row['Borough'],
+                    'Zipcode': row['Postcode'],
+                    'Latitude': row['Latitude'],
+                    'Longitude': row['Longitude'],
+                    'CommunityBoard': row['CommunityBoard'],
+                    'CouncilDistrict': row['CouncilDistrict'],
+                    'CensusTract': row['CensusTract'],
+                    'BIN': row['BIN'],
+                    'BBL': row['BBL'],
+                    'NTA': row['NTA']
+                })
+
+        tables['Restaurant'] = pd.concat(
+            [tables['Restaurant'], pd.DataFrame(to_add)], ignore_index=True)
+        datasets['OpenRestaurantInspections']['RestaurantID'] = restaurant_ids
+
+
+def fillSidewalkInspectionTable(tables: dict[str, pd.DataFrame], df: pd.DataFrame) -> None:
+    '''
+    Fill the Restaurant table
+    '''
+
+    if 'OpenRestaurantInspections' in datasets:
+        df = datasets['OpenRestaurantInspections']
+
+        to_add = []
+
+        for _, row in tqdm(df.iterrows()):
+
+            to_add.append({
+                'ID': str(generateRandomBits(64)),
+                'RestaurantID': row['RestaurantID'],
+                'InspectedOn': row['InspectedOn'],
+                'SidewayCompliant': row['SidewayCompliant'],
+                'SkippedReason': row['SkippedReason'],
+                'AgencyCode': row['AgencyCode'],
+            })
+
+        tables['SidewalkInspection'] = pd.concat(
+        [tables['SidewalkInspection'], pd.DataFrame(to_add)], ignore_index=True)
+
+
+def fillTables(datasets: dict[str, pd.DataFrame], tables: dict[str, pd.DataFrame]) -> None:
+
+    fillRestaurantTable(tables, datasets)
+    fillSidewalkInspectionTable(tables, datasets)
+
+
+def editData(filename: str, dfs: dict[str, pd.DataFrame]) -> dict[str, pd.DataFrame]:
+    '''
+    Make edits to the raw data
+    '''
+
+    # Open the file
+    with open(filename) as file:
+        raw_edits = yaml.load(file, Loader=yaml.FullLoader)
+
+    # Iterate through each edit
+    for edit in raw_edits:
+
+        # Convert X_name.csv to name
+        key = edit['file'].split('_')[1].split('.')[0]
+
+        if key in dfs:
+            for row in edit['row']:
+                dfs[key].loc[row, edit['column']] = edit['value']
+
+    return dfs
+
+
+def generateRandomString(length=primaryKeyLength) -> str:
+    '''
     Generate a random string of length characters
     '''
     return ''.join(random.choices(string.ascii_letters + string.digits, k=length))
+
+
+def generateRandomBits(length: int) -> int:
+    '''
+    Generate a random number
+    '''
+
+    return random.randint(0, 2**length-1)
 
 
 def assignBranchID(df: pd.DataFrame, verbose=False) -> pd.DataFrame:
@@ -73,6 +186,53 @@ def formatOpenRestaurantInspections(df: pd.DataFrame) -> pd.DataFrame:
     OpenRestaurantInspections.csv file
     '''
 
+    ###################
+    # Restaurant Name #
+    ###################
+
+    # Standardize the RestaurantName
+    df = format.standardizeString(
+        df, {'RestaurantName': 'FormattedRestaurantName'})
+
+    ###############
+    # InspectedOn #
+    ###############
+
+    # Validate that all values in InspectedOn are convertable to a datetime and
+    # convert the date to UTC
+
+    # Timezone in NYC
+    eastern = pytz.timezone('US/Eastern')
+
+    try:
+        inspected_on = pd.to_datetime(
+            df['InspectedOn'], errors='raise').dt.tz_localize(eastern)
+        df['InspectedOn'] = inspected_on.dt.tz_convert(pytz.utc)
+    except:
+        print('Cannot convert all values in InspectedOn to a date')
+
+    #########################################
+    # IsRoadwayCompliant/IsSidewayCompliant #
+    #########################################
+
+    # Drop IsSideWalkCompliant and rename IsRoadwayCompliant to IsSidewayCompliant
+    df = df.drop(columns=['IsSidewayCompliant'])
+    df = df.rename(columns={'IsRoadwayCompliant': 'SidewayCompliant'})
+
+    #################
+    # SkippedReason #
+    #################
+
+    # Replaced null values with 'N/A'
+    df['SkippedReason'] = df['SkippedReason'].fillna('NA')
+
+    ###############
+    # Agency Code #
+    ###############
+
+    # Do nothing. Don't want to fill in null values with 'NA' because it will be
+    # confusing when 3 and 4 letter codes
+
     # Rename LegalBusinessName to RawLegalBusinessName
     df = df.rename(columns={'LegalBusinessName': 'RawLegalBusinessName'})
 
@@ -91,7 +251,7 @@ def formatOpenRestaurantInspections(df: pd.DataFrame) -> pd.DataFrame:
         df, {'RawBusinessAddress': 'FormattedBusinessAddress'})
 
     # Assign a BranchID to each row
-    df = assignBranchID(df, verbose=True)
+    # df = assignBranchID(df, verbose=True)
 
     return df
 
@@ -110,20 +270,42 @@ def assembleTables(datasets: dict[str, pd.DataFrame]) -> dict[str, pd.DataFrame]
     '''
     tables = {}
 
-    # Create businesses table
-    df_businesses = pd.DataFrame()
+    # Create a DataFrame from a list of column names and types
+    tables['SidewalkInspection'] = pd.DataFrame({
+        'ID': pd.Series(dtype='string'),
+        'RestaurantID': pd.Series(dtype='string'),
+        'InspectedOn': pd.Series(dtype='datetime64[ns, UTC]'),
+        'SidewayCompliant': pd.Series(dtype='string'),
+        'SkippedReason': pd.Series(dtype='string'),
+        'AgencyCode': pd.Series(dtype='string')})
 
-    df = datasets['OpenRestaurantInspections']
+    tables['Restaurant'] = pd.DataFrame({
+        'ID': pd.Series(dtype='string'),
+        'DBA': pd.Series(dtype='string'),
+        'Name': pd.Series(dtype='string'),
+        'LegalBusinessName': pd.Series(dtype='string'),
+        'StreetAddress': pd.Series(dtype='string'),
+        'Borough': pd.Series(dtype='string'),
+        'Zipcode': pd.Series(dtype='int32'),
+        'FoodServicePermit': pd.Series(dtype='int32'),
+        'IsPermittedToSellAlcohol': pd.Series(dtype='boolean'),
+        'SLASerialNumber': pd.Series(dtype='string'),
+        'SLALicenseType': pd.Series(dtype='string'),
+        'IsLandmark': pd.Series(dtype='boolean'),
+        'HasAgreedToLandmarkTerms': pd.Series(dtype='boolean'),
+        'Latitude': pd.Series(dtype='float64'),
+        'Longitude': pd.Series(dtype='float64'),
+        'CommunityBoard': pd.Series(dtype='int32'),
+        'CouncilDistrict': pd.Series(dtype='int32'),
+        'CensusTract': pd.Series(dtype='int32'),
+        'BIN': pd.Series(dtype='int32'),
+        'BBL': pd.Series(dtype='int32'),
+        'NTA': pd.Series(dtype='string'),
+        'CAMIS': pd.Series(dtype='int32'),
+        'Phone': pd.Series(dtype='string'),
+        'Cuisine': pd.Series(dtype='string')})
 
-    df_businesses['FormattedLegalBusinessName'] = df['FormattedLegalBusinessName']
-    df_businesses['FormattedBusinessAddress'] = df['FormattedBusinessAddress']
-    df_businesses['RawLegalBusinessName'] = df['RawLegalBusinessName']
-    df_businesses['RawBusinessAddress'] = df['RawBusinessAddress']
-    df_businesses['BranchID'] = df['BranchID']
-
-    df_businesses.drop_duplicates(subset=['FormattedLegalBusinessName', 'FormattedBusinessAddress'], inplace=True)
-
-    tables['businesses'] = df_businesses
+    fillTables(datasets, tables)
 
     return tables
 
@@ -169,6 +351,7 @@ else:
 # Format Rows #
 ###############
 
+editData('data/raw/edits.yaml', datasets)
 
 if 'OpenRestaurantApplications' in datasets:
     datasets['OpenRestaurantApplications'] = formatOpenRestaurantApplications(
--- format.py
+++ format.py
@@ -1,4 +1,5 @@
 import pandas as pd
+from tqdm import tqdm
 from typing import Dict
 import string
 
@@ -20,6 +21,11 @@ def standardizeString(df: pd.DataFrame, subset: Dict[str, str]) -> pd.DataFrame:
         subset: {current_column_name: new_column_name}
     '''
 
+    # Remove keep punctuation from string.punctuation
+    remove_punctuation = string.punctuation
+    for p in keep_punctuation:
+        remove_punctuation = remove_punctuation.replace(p, '')
+
     for c1, c2 in subset.items():
 
         df[c2] = df[c1].str.lower()
@@ -30,36 +36,64 @@ def standardizeString(df: pd.DataFrame, subset: Dict[str, str]) -> pd.DataFrame:
         # Replace instances of ó with o
         df[c2] = df[c2].str.replace('+ae', 'o')
 
+        # Replace instances of â with a
+        df[c2] = df[c2].str.replace('+e', 'a')
+
+        # Replace instances of á with a
+        df[c2] = df[c2].str.replace('+u', 'a')
+
         # Example v+isquez to vasquez
         df[c2] = df[c2].str.replace('+i', 'a')
+
+        # Example Frank-?s to Frank's
+        df[c2] = df[c2].str.replace('-?s', "'s")
+
+        # Hurley;s to Hurley's
+        df[c2] = df[c2].str.replace('y;s', "'s")
 
         df[c2] = df[c2].str.replace('&amp;', '&')
 
         # Remove all white space from value
         df[c2] = df[c2].str.replace(' ', '')
+        
+        df[c2] = df[c2].str.replace('-', '')
+        df[c2] = df[c2].str.replace('.', '')
+        df[c2] = df[c2].str.replace(',', '')
+        df[c2] = df[c2].str.replace("'", '')
+        df[c2] = df[c2].str.replace('#', '')
+        df[c2] = df[c2].str.replace(':', '')
+        df[c2] = df[c2].str.replace('*', '')
+        df[c2] = df[c2].str.replace(';', '')
+        df[c2] = df[c2].str.replace('+', '')
+        df[c2] = df[c2].str.replace('@', '')
+        df[c2] = df[c2].str.replace('!', '')
+        df[c2] = df[c2].str.replace('~', '')
+        df[c2] = df[c2].str.replace('[', '')
+        df[c2] = df[c2].str.replace(']', '')
+        df[c2] = df[c2].str.replace('_', '')
+        df[c2] = df[c2].str.replace('%', '')
+        df[c2] = df[c2].str.replace('?', '')
+        
+        # # Remove remaining punctuation from value
+        # for p in remove_punctuation:
+        #     df[c2] = df[c2].str.replace(p, '')
 
-        # Remove keep punctuation from string.punctuation
-        remove_punctuation = string.punctuation
-        for p in keep_punctuation:
-            remove_punctuation = remove_punctuation.replace(p, '')
-
-        # Remove remaining punctuation from value
-        for p in remove_punctuation:
-            df[c2] = df[c2].str.replace(p, '')
-
-    # # Open a file to write to
+    # Open a file to write to
     # f = open('output.txt', 'w')
 
-    # for v in df[c2]:
+    # for index, v in df[c2].items():
 
     #     try:
-    #         if not pd.isnull(v) and any(c in string.punctuation.replace('&', '').replace('(', '').replace(')', '').replace('/', '') for c in v):
-    #             f.write(v + '\n')
-    #     except:
+    #         # if not pd.isnull(v) and any(c in [''] for c in v):
+    #         if not pd.isnull(v) and any(c in remove_punctuation for c in v):
+    #             f.write(f'{index}, {v}\n')
+    #     except Exception as e:
+    #         print(e)
     #         print(v)
     #         print(type(v))
     #         exit()
 
     # f.close()
+    # exit()
 
     return df
